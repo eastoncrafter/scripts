d6b8e310c32504796935123a6c92ef6720caf32b
eastoncrafter <74274138+eastoncrafter@users.noreply.github.com>
2022-05-23 10:40:42 -0400

    updates

--- swap.sh
+++ swap.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
+#!/bin/bash root
 #make sure to run as sudo
-if (( $EUID != 0 )); then
-    echo "Please run as root"
-    exit
-    else
+
 fallocate -l 8G /swapfile 
 chmod 600 /swapfile
 mkswap /swapfile
@@ -11,5 +8,4 @@ swapon /swapfile
 free -h
 echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
 exit
-fi
 
